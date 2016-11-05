430ff4c309a612953b3449ef1925af19461747df
Yuan <cqnkxy@gmail.com>
2016-11-05 00:54:27 -0700

    favorite pending

--- congress.php
+++ congress.php
@@ -5,16 +5,31 @@ if (isset($_GET["legislators"])){
 	$url = "https://congress.api.sunlightfoundation.com/legislators?apikey={$api_key}&per_page=all";
 	echo file_get_contents($url);
 }elseif (isset($_GET["legislator_details"])){
-	$url1 = "https://congress.api.sunlightfoundation.com/bills?apikey={$api_key}&sponsor_id={$_GET["bioguide_id"]}";
-	$url2 = "https://congress.api.sunlightfoundation.com/committees?apikey={$api_key}&member_ids={$_GET["bioguide_id"]}";
+	$bills = "https://congress.api.sunlightfoundation.com/bills?apikey={$api_key}&sponsor_id={$_GET["bioguide_id"]}";
+	$committees = "https://congress.api.sunlightfoundation.com/committees?apikey={$api_key}&member_ids={$_GET["bioguide_id"]}";
 	echo "[";
-	echo file_get_contents($url1);
+	echo file_get_contents($bills);
 	echo ",";
-	echo file_get_contents($url2);
+	echo file_get_contents($committees);
 	echo "]";
 }elseif (isset($_GET["bills"])) {
-	$url = "https://congress.api.sunlightfoundation.com/bills?apikey={$api_key}&per_page=50";
+	$active = "https://congress.api.sunlightfoundation.com/bills?apikey={$api_key}&history.active=true&per_page=50";
+	$new = "https://congress.api.sunlightfoundation.com/bills?apikey={$api_key}&history.active=false&per_page=50";
+	echo "[";
+	echo file_get_contents($active);
+	echo ",";
+	echo file_get_contents($new);
+	echo "]";
 } elseif (isset($_GET["committees"])) {
-	$url = "https://congress.api.sunlightfoundation.com/committees?apikey={$api_key}&per_page=all";
+	$house = "https://congress.api.sunlightfoundation.com/committees?chamber=house&apikey={$api_key}&per_page=all";
+	$senate = "https://congress.api.sunlightfoundation.com/committees?chamber=senate&apikey=d4fa8a29d3784250bf6269e00c75d893&per_page=all";
+	$joint = "https://congress.api.sunlightfoundation.com/committees?chamber=joint&apikey={$api_key}&per_page=all";
+	echo "[";
+	echo file_get_contents($house);
+	echo ",";
+	echo file_get_contents($senate);
+	echo ",";
+	echo file_get_contents($joint);
+	echo "]";
 }
 ?>
--- script.js
+++ script.js
@@ -3,55 +3,75 @@ var myApp = angular.module('myApp',
     'ngAnimate', 'ngSanitize', 'ui.bootstrap']
 );
 
-myApp.service("legislatorsService", function() {
-    var legislators = [];
-    var legislatorsService = {};
+function localStorageGet(key) {
+    if (!localStorage[key]) {
+        return [];
+    } else {
+        return JSON.parse(localStorage[key]);
+    }
+}
 
-    legislatorsService.list = function() {
-        return legislators;
-    };
-
-    return legislatorsService;
-});
-
-function PageController($scope) {
-    $scope.pageChangeHandler = function(num) {
-        console.log('going to page ' + num);
-    };
+function localStoragePush(key, val) {
+    var vec = localStorage[key];
+    if (!vec) {
+        vec = [];
+    } else {
+        vec = JSON.parse[vec];
+    }
+    vec.push(val);
+    localStorage[key] = JSON.stringify[vec];
 }
 
 /************** lagislators ****************/
 
-function parse_legislator(tmp) {
-    if (tmp.party == 'R') {
-        tmp.party = {
+myApp.service("legislatorsService", function() {
+    var legislators = [];
+    var houses = [];
+    var senates = [];
+    var legislatorsService = {};
+    legislatorsService.list = function() {
+        return legislators;
+    };
+    legislatorsService.gethouses = function() {
+        return houses;
+    }
+    legislatorsService.getsenates = function() {
+        return senates;
+    }
+    return legislatorsService;
+});
+
+function parse_legislator(l) {
+    if (l.party == 'R') {
+        l.party = {
             src: "http://cs-server.usc.edu:45678/hw/hw8/images/r.png",
             name: "Republic",
         };
-    } else if (tmp.party == 'D') {
-        tmp.party = {
+    } else if (l.party == 'D') {
+        l.party = {
             src: "http://cs-server.usc.edu:45678/hw/hw8/images/d.png",
             name: "Democrat",
         };
     } else {
-        tmp.party = {
+        l.party = {
             src: "http://independentamericanparty.org/wp-content/themes/v/images/logo-american-heritage-academy.png",
             name: "Independent",
         };
     }
-    if (tmp.chamber == "house") {
-        tmp.chamber = {
+    if (l.chamber == "house") {
+        l.chamber = {
             src: "http://cs-server.usc.edu:45678/hw/hw8/images/h.png",
             name: "House",
         };
     } else {
-        tmp.chamber = {
+        l.chamber = {
             src: "http://cs-server.usc.edu:45678/hw/hw8/images/s.svg",
             name: "Senate",
         }
     }
-    tmp.district = tmp.district ? "Disctrict " + tmp.district : "N.A";
-    return tmp;
+    l.district = l.district ? "Disctrict " + l.district : "N.A";
+    l.favorite = false;
+    return l;
 }
 
 function sort_state_lastname(legislators) {
@@ -77,6 +97,21 @@ function legislatorsGetController($scope, $http, legislatorsService) {
                     legislators.push(tmp);
                 }
                 sort_state_lastname(legislators);
+                houses = legislatorsService.gethouses();
+                senates = legislatorsService.getsenates();
+                legislators.forEach(function(e){
+                    if (e.chamber.name == "House") {
+                        houses.push(angular.copy(e));
+                    } else {
+                        senates.push(angular.copy(e));
+                    }
+                });
+                houses.sort(function(a, b) {
+                    return a.last_name.localeCompare(b.last_name);
+                });
+                senates.sort(function(a, b) {
+                    return a.last_name.localeCompare(b.last_name);
+                });
             });
         }
     };
@@ -85,48 +120,201 @@ function legislatorsGetController($scope, $http, legislatorsService) {
 
 function legislatorsViewController($scope, $http, legislatorsService) {
     $scope.currentPage = 1;
-    $scope.pageSize = 10;
     $scope.legislators = legislatorsService.list();
+    $scope.houses = legislatorsService.gethouses();
+    $scope.senates = legislatorsService.getsenates();
     $scope.specific_legislator = {};
     $scope.details = function(l) {
         $scope.progress = Math.round(100*(new Date() - new Date(l.term_start))
             / (new Date(l.term_end) - new Date(l.term_start)));
         $scope.specific_legislator = l;
-        console.log(l);
         $http.get(`congress.php?legislator_details=true&bioguide_id=${l.bioguide_id}`)
         .then(function(response) {
-            console.log(response);
             $scope.bills = response.data[0].results.splice(0, 5);
             $scope.committees = response.data[1].results.splice(0, 5);
-            console.log($scope.committees);
-            console.log($scope.bills);
         });
+    };
+    $scope.favorite = function(l) {
+        l.favorite = true;
+        localStoragePush("legislator", l);
     };
 }
 
+/************** #lagislators ***************/
+/****************** bills ******************/
 
-/******************** bills ***********************/
+myApp.service("billsService", function(){
+    var bills = [];
+    var active_bills = [];
+    var new_bills = [];
+    var billsService = {};
+    billsService.list = function() {
+        return bills;
+    };
+    billsService.getNewBills = function() {
+        return new_bills;
+    };
+    billsService.getActiveBills = function() {
+        return active_bills;
+    }
+    return billsService;
+});
 
-function billsGetController($scope, $http) {
-
+function parse_bill(bill, active) {
+    if (bill.chamber == "house") {
+        bill.chamber = {
+            src: "http://cs-server.usc.edu:45678/hw/hw8/images/h.png",
+            name: "House",
+        };
+    } else {
+        bill.chamber = {
+            src: "http://cs-server.usc.edu:45678/hw/hw8/images/s.svg",
+            name: "Senate",
+        }
+    }
+    if (active) {
+        bill.active_or_new = "active";
+    } else {
+        bill.active_or_new = "new";
+    }
+    bill.bill_type = bill.bill_type.toUpperCase();
+    bill.favorite = false;
+    return bill;
 }
 
-/******************** committees ***********************/
-
-function committeesGetController($scope, $http) {
-
+function billsGetController($scope, $http, billsService) {
+    var bill_get = function() {
+        active_bills = billsService.getActiveBills();
+        new_bills = billsService.getNewBills();
+        if (active_bills.length == 0) {
+            $http.get("congress.php?bills=true")
+            .then(function(response) {
+                var vec_active = response.data[0].results.splice(0, 50);
+                var vec_new = response.data[1].results.splice(0, 50);
+                for (var i=0; i<vec_active.length; i++) {
+                    active_bills.push(parse_bill(vec_active[i], true));
+                    new_bills.push(parse_bill(vec_new[i], false));
+                }
+            });
+        }
+    };
+    angular.element(document).ready(bill_get);
 }
 
-/******************** favorites ***********************/
+function billsViewController($scope, billsService) {
+    $scope.active_bills = billsService.getActiveBills();
+    $scope.new_bills = billsService.getNewBills();
+    $scope.details = function(bill) {
+        // console.log(bill);
+        $scope.specific_bill = bill;
+    }
+    $scope.favorite = function(bill) {
+        bill.favorite = true;
+        localStoragePush("bill", bill);
+    }
+}
+
+/****************** #bills *****************/
+/*************** committees ****************/
+
+myApp.service("committeesService", function(){
+    var c_houses = [];
+    var c_senates = [];
+    var c_joints = [];
+    var committeesService = {};
+    committeesService.getHouses = function() {
+        return c_houses;
+    };
+    committeesService.getSenates = function() {
+        return c_senates;
+    };
+    committeesService.getJoints = function() {
+        return c_joints;
+    };
+    return committeesService;
+});
+
+function sort_id(committees) {
+    committees.sort(function(a, b){
+        return a.committee_id.localeCompare(b.committee_id);
+    });
+}
+
+function parse_committee(c) {
+    if (!c.office) {
+        c.office = "N.A";
+    }
+    if (c.chamber == "house") {
+        c.chamber = {
+            src: "http://cs-server.usc.edu:45678/hw/hw8/images/h.png",
+            name: "House",
+        };
+    } else {
+        c.chamber = {
+            src: "http://cs-server.usc.edu:45678/hw/hw8/images/s.svg",
+            name: "Senate",
+        }
+    }
+    c.favorite = false;
+    return c;
+}
+
+function committeesGetController($scope, $http, committeesService) {
+    var committee_get = function() {
+        // Unexpected name collision if I name it simply houses in the
+        //  $http call back function
+        c_houses = committeesService.getHouses();
+        c_senates = committeesService.getSenates();
+        c_joints = committeesService.getJoints();
+        if (c_houses.length == 0) {
+            $http.get("congress.php?committees=true")
+            .then(function(response) {
+                var h = response.data[0].results;
+                var s = response.data[1].results;
+                var j = response.data[2].results;
+                h.forEach(function(e){
+                    c_houses.push(parse_committee(e));
+                });
+                s.forEach(function(e){
+                    c_senates.push(parse_committee(e));
+                });
+                j.forEach(function(e){
+                    c_joints.push(parse_committee(e));
+                });
+                sort_id(c_houses);
+                sort_id(c_senates);
+                sort_id(c_joints);
+                console.log(c_joints);
+            });
+        }
+    };
+    angular.element(document).ready(committee_get);
+}
+
+function committeesViewController($scope, committeesService) {
+    $scope.houses = committeesService.getHouses();
+    $scope.senates = committeesService.getSenates();
+    $scope.joints = committeesService.getJoints();
+    $scope.favorite = function(c) {
+        c.favorite = true;
+        localStoragePush("committee", c);
+    };
+}
+
+/*************** #committees ***************/
+/************** favorites ******************/
 
 function favoritesGetController($scope, $http) {
 
 }
 
+/************** #favorites *****************/
+
 
 myApp.controller('legislatorsGetController', legislatorsGetController);
 myApp.controller('legislatorsViewController', legislatorsViewController);
 myApp.controller('billsGetController', billsGetController);
+myApp.controller('billsViewController', billsViewController);
 myApp.controller('committeesGetController', committeesGetController);
+myApp.controller('committeesViewController', committeesViewController);
 myApp.controller('favoritesGetController', favoritesGetController);
-myApp.controller('PageController', PageController);
